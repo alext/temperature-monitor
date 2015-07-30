b33607331e2cf29cfe08770e88298fcdb389da5f
Alex Tomlins <alex@tomlins.org.uk>
2015-07-30 21:12:34 +0100

    Main function to test sensor on real device.

--- main.go
+++ main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/alext/temperature-monitor/sensor"
+)
+
 func main() {
+	sensorID := flag.String("sensorid", "", "The ID of the sensor device")
+
+	flag.Parse()
+
+	if *sensorID == "" {
+		log.Fatal("no sensorid provided")
+	}
+
+	sensor, err := sensor.New(*sensorID)
+	if err != nil {
+		log.Fatal("Error opening sensor : ", err)
+	}
+
+	for {
+		temp := sensor.Temperature()
+		fmt.Printf("Temperature: %.3f\n", temp)
+
+		time.Sleep(10 * time.Second)
+	}
 }
