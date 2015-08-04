e268d7d5c4537f97b6899c79fa4dc64e6ae6901c
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-08-04 16:27:45 +0100

    Add flag to control log destination

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/alext/temperature-monitor/sensor"
@@ -11,8 +12,11 @@ import (
 
 func main() {
 	sensorID := flag.String("sensorid", "", "The ID of the sensor device")
+	logDest := flag.String("log", "STDERR", "Where to log to - STDOUT, STDERR or a filename")
 
 	flag.Parse()
+
+	setupLogging(*logDest)
 
 	if *sensorID == "" {
 		log.Fatal("no sensorid provided")
@@ -28,5 +32,20 @@ func main() {
 		fmt.Printf("Temperature: %.3f\n", temp)
 
 		time.Sleep(10 * time.Second)
+	}
+}
+
+func setupLogging(destination string) {
+	switch destination {
+	case "STDERR":
+		log.SetOutput(os.Stderr)
+	case "STDOUT":
+		log.SetOutput(os.Stdout)
+	default:
+		file, err := os.OpenFile(destination, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0600)
+		if err != nil {
+			log.Fatalf("Error opening log %s: %s", destination, err.Error())
+		}
+		log.SetOutput(file)
 	}
 }
